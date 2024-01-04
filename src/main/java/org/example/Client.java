7597f71eace17dcd88324ea4015b48c437e75bfb
udarasan <udarassanjeewa@gmail.com>
2024-01-04 22:49:24 +0530

    Initial commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,5 @@
+package org.example;/**
+ * @TimeStamp 2024-01-04 22:35
+ * @ProjectDetails ChatApplication02
+ */public class Client {
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @TimeStamp $DATE $TIME
+ * @ProjectDetails $PROJECT_NAME
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
